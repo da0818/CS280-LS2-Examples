3aea2d4365b2784f5604524984174d49fe3071f5
da0818 <chuntawu@gmail.com>
2017-03-07 19:56:23 +0800

    完成簡單登入程式

---
+++ CS280-LS2-Examples/CS280-LS2-Example02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_LS2_Example02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
