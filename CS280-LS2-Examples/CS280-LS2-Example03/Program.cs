4eef1838f2303bb1abb76297b0826dec7c6e3fa3
da0818 <chuntawu@gmail.com>
2017-03-07 20:47:01 +0800

    string字串問題

---
+++ CS280-LS2-Examples/CS280-LS2-Example03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_LS2_Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql =
+                @"select * from table
+                 where id > 1000";
+            string @"<Pokemon>
+                       <NationalNo>#001</NationalNo>
+                       <Name>Bulbsaur</Name>
+                     </Pokemon>";
+            string json =
+                @"Pokemon = {
+                    NationalNo: '#001' ,
+                    Name:Bulbsaur
+                    }";
+        }
+    }
+}
